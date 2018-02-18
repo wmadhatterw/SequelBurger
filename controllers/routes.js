1acd4fdadc990609e5312d3a4f98e1a879c9868b
Seth Walton <sdwalton83@gmail.com>
2018-02-18 15:11:28 -0600

    switching to config.json

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,8 +1,12 @@
 var express = require("express");
 var router = express.Router();
+var burger = require("../models/burger.js")
 
 router.get('/',function(req, res){
-	res.render('index');
+	burger.all(function(burger_data){
+		console.log(burger_data);
+	res.render('index')
+	})
 })
 
 module.exports = router;
---
+++ models/burger.js
@@ -0,0 +1,10 @@
+var orm = require('../config/orm.js');
+
+var burger = {
+	all: function(cb){
+		orm.all('burgers',function(res){
+			cb(res);
+		})
+	}
+}
+module.exports = burger;
